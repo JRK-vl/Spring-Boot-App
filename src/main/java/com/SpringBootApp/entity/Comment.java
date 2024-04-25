4960c766dda210b3ae0ead6308a1e617cde51c8e
Valery Lazavitsky <valerylozovitsky@gmail.com>
2024-04-25 10:05:29 +0300

    fixed pr comments

--- src/main/java/com/SpringBootApp/entity/Comment.java
+++ src/main/java/com/SpringBootApp/entity/Comment.java
@@ -1,14 +1,12 @@
 package com.SpringBootApp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/SpringBootApp/entity/User.java
+++ src/main/java/com/SpringBootApp/entity/User.java
@@ -1,13 +1,11 @@
 package com.SpringBootApp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
